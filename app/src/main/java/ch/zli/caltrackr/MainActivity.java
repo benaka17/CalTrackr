f84b25ed2e6d42b3965804fc5147d7ed0e54acb9
benaka17 <benaka@bzz.ch>
2023-07-06 09:21:59 +0200

    MainActivity view done and variables initialized

--- app/src/main/java/ch/zli/caltrackr/MainActivity.java
+++ app/src/main/java/ch/zli/caltrackr/MainActivity.java
@@ -3,12 +3,24 @@ package ch.zli.caltrackr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView progress, steps, usedCals;
+    public Button entry, goal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progress = findViewById(R.id.progress);
+        steps = findViewById(R.id.steps);
+        usedCals = findViewById(R.id.usedCals);
+
+        entry = findViewById(R.id.entry);
+        goal = findViewById(R.id.goal);
     }
 }
